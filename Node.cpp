9657c90cf691fe14dbf4c36e7999bd2e3e75f728
bcgood <bengood98@gmail.com>
2023-12-04 18:33:09 -0500

    First commit on this branch

--- Node.cpp
+++ Node.cpp
@@ -7,381 +7,105 @@ using namespace std;
 
 Node::Node()
 {
-	Node::degree = 0;
-	Node::data;
-	//Node* firstNode = nullptr;
-	Node::links;
-	Node::count = 0;
-	Node::values;
 	Node::isLeaf = false;
-	
 }
 
-bool Node::insert(int value, std::string data)
+vector<int> sortNewKey(vector<int> keys, int newKey)
 {
-	if (isLeaf && Node::count < Node::degree) {
-		bool inserted = false;
-		int pos = 0;
-		for (auto i = Node::values.begin(); i != Node::values.end(); ++i) {
-			if (value < *i) {
-				Node::values.insert(Node::values.begin() + pos, value);
-				Node::data.insert(Node::data.begin() + pos, data);
-				inserted = true;
-				break;
-			}
-			pos++;
-		}
-		if (!inserted) {
-			Node::values.push_back(value);
-			Node::data.push_back(data);
-		}
-		Node::count++;
-
-		return true;
-	}
-	else if (!isLeaf) {
-		bool inserted = false;
-		int pos = 0;
-		for (auto i = Node::values.begin(); i != Node::values.end(); ++i) {
-			if (value < *i) {
-				inserted = Node::links.at(pos)->insert(value, data);
-				// TODO: Node splitting calls can be placed in this block
-				inserted = true;
-				break;
-			}
-			pos++;
-		}
-		if (!inserted) {
-			inserted = Node::links.at(pos + 1)->insert(value, data);
-			// It appears node splitting calls will be needed here as well
-		}
-		Node::count++;
-
-		return true;
-	}
-	return false;
+	keys.push_back(newKey);
+	sort(keys.begin(), keys.end());
+	return keys;
 }
 
-bool Node::checkLeaf()
+dataSplit splitDataNode(Node* node, int key, string data)
 {
-	return Node::isLeaf;
-}
+	vector<int> allKeys = sortNewKey(node->keys, key);
+	int middlePos = (int)allKeys.size() / 2;
+	int middleValue = allKeys.at(middlePos);
+	int degree = node->keys.size();
 
-void Node::setLeaf(bool isLeaf)
-{
-	Node::isLeaf = isLeaf;
-}
+	Node* left = new Node();
+	Node* right = new Node();
+	left->isLeaf = true;
+	right->isLeaf = true;
 
-void Node::setDegree(int degree)
-{
-	Node::degree = degree;
-}
-
-int Node::getDegree()
-{
-	return Node::degree;
-}
-
-int Node::getValue(int position)
-{
-	if (Node::count == 0) return -1;
-	return Node::values.at(position);
-}
-
-void Node::setValue(int value, int position)
-{
-	Node::values[position] = value;
-}
-
-int Node::valueInsertion(int value)
-{
-	int i;
-	bool inserted = false;
-	if (Node::count == 0) Node::count = 1;
-	for (i = 0; i < Node::count; ++i) {
-		if (Node::values.at(i) > value) {
-			Node::values.insert(Node::values.begin() + i, value);
-			Node::count++;
-			inserted = true;
-			break;
-		}
-	}
-	if (Node::count == 1) Node::count--;
-
-	if (!inserted && Node::count < Node::degree) {
-		Node::values.push_back(value);
-		Node::count++;
+	bool* inserted = new bool[node->data.size()];
+	for (int i = 0; i < node->data.size(); ++i) {
+		inserted[i] = false;
 	}
 
-	return i;
-}
-
-void Node::setData(std::string data, int position)
-{
-	Node::data[position] = data;
-}
-
-int Node::getCount()
-{
-	return Node::count;
-}
-
-std::string Node::getData(int position)
-{
-	return Node::data[position];
-}
-
-void Node::setLink(Node* node, int position)
-{
-	Node::links[position] = node;
-}
-
-void Node::addLink(Node* node, int position)
-{
-	if (position == -1) {
-		Node::links.push_back(node);
-	}
-	else {
-		Node::links.insert(Node::links.begin() + position, node);
-	}
-}
-
-int Node::arrangeLink(Node* node)
-{
-	int firstVal = node->getValue(0);
 	int pos = 0;
-	bool inserted = false;
-	for (auto i = Node::values.begin(); i != Node::values.end(); ++i) {
-		if (*i > firstVal) {
-			Node::links.insert(Node::links.begin() + pos, node);
-			inserted = true;
-			break;
+	for (auto i = node->keys.begin(); i != node->keys.end(); ++i) {
+		if (*i < middleValue && pos < degree) {
+			left->keys.push_back(*i);
+			for (int j = 0; j < degree; ++j) {
+				if (node->data.at(j)->key == *i && !inserted[pos]) {
+					/*
+					BPlusData* data = new BPlusData();
+					data->key = *i;
+					data->data = node->data.at(j)->data;
+					*/
+					left->data.push_back(node->data.at(j));
+					inserted[pos] = true;
+				}
+			}
 		}
-
+		else {
+			right->keys.push_back(*i);
+			for (int j = 0; j < degree; ++j) {
+				if (node->data.at(j)->key == *i && !inserted[pos]) {
+					/*
+					BPlusData* data = new BPlusData();
+					data->key = *i;
+					data->data = node->data.at(j)->data;
+					*/
+					right->data.push_back(node->data.at(j));
+					inserted[pos] = true;
+				}
+			}
+		}
 		pos++;
 	}
 
-	if (!inserted) {
-		Node::links.insert(Node::links.begin() + pos, node);
-		inserted = true;
-	}
-
-	return pos;
-}
-
-void Node::pushValue(int value)
-{
-	Node::values.push_back(value);
-	Node::count++;
-	sort(Node::values.begin(), Node::values.end());
-}
-
-Node* Node::getLink(int position)
-{
-	return Node::links[position];
-}
-
-void Node::linkInsertion(int value, int position, Node* left, Node* right)
-{
-	Node::values.insert(Node::values.begin() + position, value);
-	Node::links.insert(Node::links.begin() + position, left);
-	Node::links.insert(Node::links.begin() + position + 1, right);
-}
-
-void Node::pushBack(int ind)
-{
-	for (int i = Node::degree - 2; i >= ind; --i) {
-		Node::values[i + 1] = Node::values[i];
-	}
-
-	if (Node::checkLeaf()) {
-		for (int i = Node::degree - 2; i >= ind; --i) {
-			Node::data[i + 1] = Node::data[i];
-		}
+	if (key < middleValue) {
+		left->keys.push_back(key);
+		BPlusData* newData = new BPlusData();
+		newData->key = key;
+		newData->data = data;
+		left->data.push_back(newData);
 	}
 	else {
-		for (int i = Node::degree - 1; i >= ind; --i) {
-			Node::links[i + 1] = Node::links[i];
-		}
+		right->keys.push_back(key);
+		BPlusData* newData = new BPlusData();
+		newData->key = key;
+		newData->data = data;
+		right->data.push_back(newData);
 	}
+
+	dataSplit ds;
+	ds.nodes.push_back(left);
+	ds.nodes.push_back(right);
+	ds.middleValue = middleValue;
+
+	return ds;
 }
 
-dataSplit splitDataNode(Node* node, int value, std::string data, int rootValue)
+internalSplit splitInternalNode(Node* node, Node* left, Node* right, int key)
 {
-	Node* first = new Node();
-	Node* second = new Node();
-	int middlePos = (int)node->getCount() / 2;
-	int pivotValue = node->getValue(middlePos);
-	int degree = node->getDegree();
-
-	first->setDegree(degree);
-	first->setLeaf(true);
-	second->setDegree(degree);
-	second->setLeaf(true);
-
-	for (int i = 0; i <= degree; ++i) {
-		if (i == degree) {
-			if (value > pivotValue) {
-				second->insert(value, data);
-			}
-			else if (value == pivotValue && pivotValue >= rootValue) {
-				second->insert(value, data);
-			}
-			else {
-				first->insert(value, data);
-			}
-		}
-		else {
-			if (node->getValue(i) < pivotValue) {
-				first->insert(node->getValue(i), node->getData(i));
-			}
-			else if (node->getValue(i) == pivotValue && pivotValue < rootValue) {
-				first->insert(node->getValue(i), node->getData(i));
-			}
-			else {
-				second->insert(node->getValue(i), node->getData(i));
-			}
-		}
-	}
-
-	dataSplit result;
-
-	result.nodes.push_back(first);
-	result.nodes.push_back(second);
-	result.middleValue = pivotValue;
-
-	return result;
-}
-internalSplit splitInternalNode(Node* node, Node* left, Node* right, int newValue, int oldLink)
-{
-	int degree = node->getDegree();
-	vector<int> allVals = orderValues(node, newValue, degree + 1);
-	newValues pivotData = getPivot(allVals);
-	vector<Node*> newLinks = getNewLinks(node, left, right, oldLink);
-	newLinks = sortLinks(newLinks);
-
-	// TODO
-	return internalSplit();
-}
-
-vector<Node*> arrangeLinks(vector<Node*> nodes) {
-	vector<int> firstVals;
-	int nSize = nodes.size();
-
-	for (int i = 0; i < nSize; ++i) {
-		firstVals.push_back(nodes.at(i)->getValue(0));
-	}
-
-	for (int i = 0; i < nSize; ++i) {
-		for (int j = i+1; j < nSize; ++j) {
-			if (firstVals.at(j) < firstVals.at(i)) {
-				iter_swap(firstVals.begin() + i, firstVals.begin() + j);
-				iter_swap(nodes.begin() + i, nodes.begin() + j);
-			}
-		}
-	}
-
-	return nodes;
-}
-
-/*internalSplit splitInternalNode(Node* parent, Node* left, Node* right, int newValue, int oldLink) {
-	Node* first = new Node();
-	Node* second = new Node();
-	int degree = parent->getDegree();
-	vector<int> allVals = orderValues(parent, newValue, degree + 1);
-	int middlePos = (int)allVals.size() / 2;
-	vector<Node*> allLinks;
-	
-	// filling allLinks
-	for (int i = 0; i <= degree; ++i) {
-		if (i != oldLink) {
-			allLinks.push_back(parent->getLink(i));
-		}
-	}
-	allLinks.push_back(left);
-	allLinks.push_back(right);
-	vector<Node*> arrangedLinks = arrangeLinks(allLinks);
-
-	first->setDegree(degree);
-	first->setLeaf(false);
-	second->setDegree(degree);
-	second->setLeaf(false);
+	vector<int> allKeys = sortNewKey(node->keys, key);
+	int middlePos = (int)allKeys.size() / 2;
+	int middleValue = allKeys.at(middlePos);
+	int degree = node->keys.size();
 
 	int i;
-	for (i = 0; i < middlePos; ++i) {
-		first->pushValue(allVals.at(i));
-		first->addLink(arrangedLinks.at(i), -1);
-
-	}
-	first->addLink(arrangedLinks.at(i + 1), -1);
-
-	int j;
-	for (j = middlePos + 1; j < allVals.size(); ++j) {
-		second->pushValue(allVals.at(j));
-		second->addLink(arrangedLinks.at(j), -1);
-	}
-	second->addLink(arrangedLinks.at(j + 1), -1);
-
-	internalSplit result;
-	result.nodes.push_back(first);
-	result.nodes.push_back(second);
-	result.value = allVals.at(middlePos);
-
-	return result;
-}
-*/
-
-vector<int> orderValues(Node* n, int newValue, int total)
-{
-	vector<int> cumulative;
-	for (int i = 0; i < total; ++i) {
-		if (i != total - 1) {
-			cumulative.push_back(n->getValue(i));
+	for (i = 0; i < allKeys.size(); ++i) {
+		if (allKeys.at(i) < middleValue) {
+			left->keys.push_back(allKeys.at(i));
 		}
-		else {
-			cumulative.push_back(newValue);
+		else if (allKeys.at(i) > middleValue) {
+			right->keys.push_back(allKeys.at(i));
 		}
 	}
-	sort(cumulative.begin(), cumulative.end());
-	return cumulative;
-}
 
-newValues getPivot(vector<int> values)
-{
-	int middlePos = (int)values.size() / 2;
-	int pivotValue = values.at(middlePos);
-	values.erase(values.begin() + middlePos);
-
-	newValues result;
-	result.values = values;
-	result.pivot = pivotValue;
-	return result;
-}
-
-vector<Node*> sortLinks(vector<Node*> links)
-{
-	int firstVal;
-	int otherVal;
-	for (int i = 0; i < links.size() - 1; ++i) {
-		firstVal = links.at(i)->getValue(0);
-		for (int j = i + 1; j < links.size(); ++j) {
-			otherVal = links.at(j)->getValue(0);
-			if (otherVal < firstVal) {
-				iter_swap(links.begin() + i, links.begin() + j);
-			}
-		}
-	}
-	return links;
-}
-
-vector<Node*> getNewLinks(Node* n, Node* left, Node* right, int oldLink)
-{
-	vector<Node*> allLinks;
-	for (int i = 0; i < n->getDegree() + 1; ++i) {
-		if (i == oldLink) continue;
-		allLinks.push_back(n->getLink(i));
-	}
-	allLinks.push_back(left);
-	allLinks.push_back(right);
-	return vector<Node*>();
+	return internalSplit();
 }
--- Node.h
+++ Node.h
@@ -3,37 +3,29 @@
 #include <vector>
 
 using namespace std;
+
+class BPlusData {
+public:
+	int key;
+	string data;
+};
+
+template <class T>
+class Link {
+public:
+	int leftOf;
+	int rightOf;
+	T* ptr;
+};
+
 class Node
 {
 public:
 	Node();
-	bool insert(int value, std::string data);
-	void delKey(int value);
-	string search(int value);
-	bool checkLeaf();
-	void setLeaf(bool isLeaf);
-	void setDegree(int degree);
-	int getDegree();
-	int getValue(int position);
-	void setValue(int value, int position);
-	int valueInsertion(int value);
-	void setData(std::string data, int position);
-	int getCount();
-	string getData(int position);
-	void setLink(Node* node, int position);
-	void addLink(Node* node, int position);
-	int arrangeLink(Node* node);
-	void pushValue(int value);
-	Node* getLink(int position);
-	void linkInsertion(int value, int position, Node* left, Node* right);
-private:
-	int degree;
-	vector<string> data;
-	vector<Node*> links;
-	vector<int> values;
-	int count;
+	vector<int> keys;
+	vector<BPlusData*> data;
+	vector<Link<Node>> links;
 	bool isLeaf;
-	void pushBack(int ind);
 };
 
 struct dataSplit {
@@ -50,11 +42,7 @@ struct newValues {
 	int pivot;
 };
 
-vector<Node*> arrangeLinks(vector<Node*> nodes);
-dataSplit splitDataNode(Node* node, int value, string data, int rootValue);
-internalSplit splitInternalNode(Node* node, Node* left, Node* right, int newValue, int oldLink);
-newValues getPivot(vector<int> values);
-vector<int> orderValues(Node* n, int newValue, int total);
-vector<Node*> getNewLinks(Node* n, Node* left, Node* right, int oldLink);
-vector<Node*> sortLinks(vector<Node*> links);
+vector<int> sortNewKey(vector<int> keys, int newKey);
+dataSplit splitDataNode(Node* node, int key, string data);
+internalSplit splitInternalNode(Node* node, Node* left, Node* right, int value);
 
